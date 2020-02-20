4c2e5e927f397297a365368a3aea9db596d038d3
Ihsan <ihsanmpm@gmail.com>
2020-02-20 22:33:24 +0530

    Update Problem.java

--- Problem.java
+++ Problem.java
@@ -3,17 +3,21 @@ class Solution {
         int plusOrMinus = 1,
         int b = 0;
         int i = 0;
-        while (str.charAt(i) == ' ') {
-            i++;
-        }
-        if (str.charAt(i) == '-' || str.charAt(i) == '+') {
-            plusOrMinus = str.charAt(i++) == '-' ? -1: 1);
-        }
-        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
-            if ((plusOrMinus > Integer.MAX_VALUE / 10) || 
-                    (plusOrMinus > Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7)) {
-                return (plusOrMinus == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            while (str.charAt(i) == ' ') {
+                i++;
+            }
+            if (str.charAt(i) == '-' || str.charAt(i) == '+') {
+                plusOrMinus = str.charAt(i++) == '-' ? -1: 1;
+            }
+            while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                if ((plusOrMinus > Integer.MAX_VALUE / 10) || 
+                        (plusOrMinus > Integer.MAX_VALUE / 10 &&
+                                    (str.charAt(i) - '0' > 7)) {
+                    return (plusOrMinus == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                }
+                b = 10 * b + (str.charAt(i++) - '0');
             }
         }
-    }   
+        return  b * plusOrMinus;           
+    }
 }
