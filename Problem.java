c477e6dacb42f514c994a69ed3dbdbc55d22f3d9
Ihsan <ihsanmpm@gmail.com>
2020-02-20 18:28:27 +0530

    Update Problem.java

--- Problem.java
+++ Problem.java
@@ -1,1 +1,10 @@
-// z
+class Solution {
+    public static int myAtoi(String str) {
+        int plusOrMinus = 1,
+        int b = 0;
+        int i = 0;
+        while(str[i] == '-' || str[i] == '+') {
+            plusOrMinus = 1 -
+        }
+    }   
+}
