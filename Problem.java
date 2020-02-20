ee5ad06711e177254b871df105eebd6d4f287823
Ihsan <ihsanmpm@gmail.com>
2020-02-20 20:19:33 +0530

    Update Problem.java

--- Problem.java
+++ Problem.java
@@ -3,8 +3,11 @@ class Solution {
         int plusOrMinus = 1,
         int b = 0;
         int i = 0;
+        while (str[i] == ' ') {
+            i++;
+        }
         while(str[i] == '-' || str[i] == '+') {
-            plusOrMinus = 1 -
+            plusOrMinus = 1 - 2 * (str[i++] == '-');
         }
     }   
 }
