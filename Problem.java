6051dfcdc1665ff3874e14624d6499f96bcb30bb
Ihsan <ihsanmpm@gmail.com>
2020-02-20 22:57:42 +0530

    Update Problem.java

--- Problem.java
+++ Problem.java
@@ -1,17 +1,19 @@
 class Solution {
     public static int myAtoi(String str) {
+        if (str.isEmpty()) return 0;
+        
         int plusOrMinus = 1;
         int b = 0;
         int i = 0;
+        
         while (str.charAt(i) == ' ') {
             i++;
         }
         if (str.charAt(i) == '-' || str.charAt(i) == '+') {
-            plusOrMinus = str.charAt(i++) == '-' ? -1: 1;
+            plusOrMinus = str.charAt(i++) == '-' ? -1 : 1;
         }
         while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
-            if ((plusOrMinus > Integer.MAX_VALUE / 10) || 
-                    (plusOrMinus == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7)) {
+            if (b > Integer.MAX_VALUE / 10 || (b == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7)) {
                 return (plusOrMinus == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             b = 10 * b + (str.charAt(i++) - '0');
