4ea79668fa414f1c6469a50144ac00a6af397625
Ihsan <ihsanmpm@gmail.com>
2020-02-21 23:08:50 +0530

    Update Merge-Sorted-Array.java

--- Merge-Sorted-Array.java
+++ Merge-Sorted-Array.java
@@ -1,5 +1,17 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         ArrayList<Integer> merge = new ArrayList<Integer>();
+        int[] mg = new int[n + m];
+        
+        int n2 = 0;
+        int temp = 0;
+        for (int i = 0 ; i < nums1.length-1; i++) {
+            if (nums1[i] < nums2[j]) {
+                temp = nums1[i+1];
+                nums1[i+1] = nums2[j];
+                = temp;
+                j++;
+            }           
+        }
     }
 }
