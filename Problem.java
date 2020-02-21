9e53970bd47b94c7b97d577a5d3669198ee45c85
Ihsan <ihsanmpm@gmail.com>
2020-02-21 10:45:17 +0530

    Comments

--- Problem.java
+++ Problem.java
@@ -10,13 +10,16 @@ class Solution {
             i++;
         }
         if (str.charAt(i) == '-' || str.charAt(i) == '+') { // if the character is Minus Or Plus
-            plusOrMinus = str.charAt(i++) == '-' ? -1 : 1;
+            plusOrMinus = str.charAt(i++) == '-' ? -1 : 1; // if it is Minus then set to Minus / else set to Positive
         }
-        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') { // Charater is between in 0 to 9 and char index position is is less than String length
+            
+            /* if the current base = 214748364, then the last digit should be less than or equal to (<=) 7 to be a valid integer,
+            otherwise, return Integer.MAX_VALUE and ignore the rest of the characters. beacuse INT_MAX = 2147483647 */
             if (b > Integer.MAX_VALUE / 10 || (b == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7)) {
                 return (plusOrMinus == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            b = 10 * b + (str.charAt(i++) - '0');
+            b = 10 * b + (str.charAt(i++) - '0'); adding each digit to the container
         }
     return  b * plusOrMinus;     
     }
