85c8570439545b4ed0edd860d0faa097e7fbb580
Ihsan <ihsanmpm@gmail.com>
2020-02-21 19:09:00 +0530

    Update strStr.java

--- strStr.java
+++ strStr.java
@@ -1,16 +1,32 @@
 // Return the index of the first occurrence of needle in String1, or -1 if needle is not part of String2.
 class Solution {
     public int strStr(String haystack, String needle) {
+        
         if(needle.isEmpty()) return 0;
-    
-        for (int i = 0 ;    ; i++) {
-            for (int j = 0 ;    ; j++) {
-                if (j == needle.length()) return i;
-                if (i + j == haystack.length()) return -1;
-                
-                if (needle.charAt(j) != haystack.charAt(i + j))
-                    break;
-            }
+        int len = haystack.length();
+        int ndlen = needle.length();
+        
+        
+        int count = 0;
+        if (ndlen > len) {
+            return -1;
         }
+        
+        for ( int i = 0; i < len - ndlen +1; i++) {
+            for ( int j = 0; j < ndlen; j++) {
+                if (hay[j + i] == need[j]) {
+                    count++;
+                    if (count == ndlen) {
+                        return i;
+                    }
+                }
+                else {
+                    count = 0;
+                    break;
+                }
+            }
+
+        }
+    return -1;      
     }
 }
