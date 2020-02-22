906fb087ebe92a9afe71f212d18eef004d2c9ae2
Ihsan <ihsanmpm@gmail.com>
2020-02-22 22:15:58 +0530

    valueOf method

--- Fizz Buzz.java
+++ Fizz Buzz.java
@@ -5,7 +5,7 @@ class Solution {
             if (i % 3 == 0 && i % 5 == 0) fb.add("FizzBuzz");                            
             else if (i % 3 == 0) fb.add("Fizz");                 
             else if (i % 5 == 0) fb.add("Buzz");                 
-            else fb.add(Integer.toString(i));
+            else fb.add(String.valueOf(i));
         }
         return fb;
     }
