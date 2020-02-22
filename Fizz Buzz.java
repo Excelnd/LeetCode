7a340d4be2328a9dfe918ca9c3c08c44262d8911
Ihsan <ihsanmpm@gmail.com>
2020-02-22 22:14:40 +0530

    Contract

--- Fizz Buzz.java
+++ Fizz Buzz.java
@@ -2,17 +2,10 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         ArrayList<String> fb = new ArrayList<String>();
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0)
-                fb.add("FizzBuzz");                            
-            else if (i % 3 == 0) {
-                fb.add("Fizz");                 
-            }
-            else if (i % 5 == 0) {
-                fb.add("Buzz");                 
-            }  
-            else {
-                fb.add(Integer.toString(i));
-            }
+            if (i % 3 == 0 && i % 5 == 0) fb.add("FizzBuzz");                            
+            else if (i % 3 == 0) fb.add("Fizz");                 
+            else if (i % 5 == 0) fb.add("Buzz");                 
+            else fb.add(Integer.toString(i));
         }
         return fb;
     }
