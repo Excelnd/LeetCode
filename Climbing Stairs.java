f380c757f5eac7b7133412872f67135404c179a7
Ihsan <ihsanmpm@gmail.com>
2020-02-22 14:40:49 +0530

    1: Brute Force

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -1,1 +1,11 @@
 
+class Solution {
+    public int climbStairs(int n) {
+        climbing(0, n);
+    }
+    
+    private int climbing(int i, int n) {
+        if (n == 0) return count;
+        if (i == 2) return (i, i+1);
+    }
+}
