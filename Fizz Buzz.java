d16d781fa819459705940b495e66d08427bba886
Ihsan <ihsanmpm@gmail.com>
2020-02-22 22:17:12 +0530

    While

--- Fizz Buzz.java
+++ Fizz Buzz.java
@@ -1,11 +1,13 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         ArrayList<String> fb = new ArrayList<String>();
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             if (i % 3 == 0 && i % 5 == 0) fb.add("FizzBuzz");                            
             else if (i % 3 == 0) fb.add("Fizz");                 
             else if (i % 5 == 0) fb.add("Buzz");                 
             else fb.add(String.valueOf(i));
+            i++;
         }
         return fb;
     }
