8e49867b2b5adad3e2f2aa55f33f27e09bf81be8
Ihsan <ihsanmpm@gmail.com>
2020-02-22 19:01:32 +0530

    Update Climbing Stairs.java

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -4,8 +4,11 @@ class Solution {
     }
     
     private int climbing(int i, int n) {
-        if (i > n) return 0;
-        if (i == n) return 1;
+        if (i > n) 
+            return 0;
+        
+        if (i == n) 
+            return 1;
         
         return climbing(i + 1, n) + climbing(i + 2, n);
     }
