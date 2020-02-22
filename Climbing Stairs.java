65c292e4b38c67eb9ae61c35ba33c5447a6e3d0d
Ihsan <ihsanmpm@gmail.com>
2020-02-22 20:25:40 +0530

    Memoization

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -1,15 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        return climbing(0, n);
+        int memo[] = new int[n + 1];
+        return climbing(0, n, memo);
     }
     
-    private int climbing(int i, int n) {
+    private int climbing(int i, int n, int memo[]) {
         if (i > n) 
             return 0;
         
         if (i == n) 
             return 1;
         
-        return climbing(i + 1, n) + climbing(i + 2, n);
+        if (memo[i] > 0) 
+            return memo[i];
+        
+        memo[i] = climbing(i + 1, n, memo) + climbing(i + 2, n, memo);
+        
+        return memo[i];
     }
 }
