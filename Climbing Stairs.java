f2183d7a86345c9ab76977c5f3909d021f8a19bc
Ihsan <ihsanmpm@gmail.com>
2020-02-22 20:40:15 +0530

    Update Climbing Stairs.java

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -1,18 +1,13 @@
-class Solution {
+public class Solution {
     public int climbStairs(int n) {
         int memo[] = new int[n + 1];
         return climbing(0, n, memo);
     }
     
     private int climbing(int i, int n, int memo[]) {
-        if (i > n) 
-            return 0;
-        
-        if (i == n) 
-            return 1;
-        
-        if (memo[i] > 0) 
-            return memo[i];
+        if (i > n) return 0;        
+        if (i == n) return 1;        
+        if (memo[i] > 0) return memo[i];
         
         memo[i] = climbing(i + 1, n, memo) + climbing(i + 2, n, memo);
         
