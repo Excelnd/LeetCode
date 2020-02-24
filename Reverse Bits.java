c2e3c6996627f625934c6e71d8b833454464813d
Ihsan <ihsanmpm@gmail.com>
2020-02-24 17:06:15 +0530

    Update Reverse Bits.java

--- Reverse Bits.java
+++ Reverse Bits.java
@@ -4,21 +4,24 @@ public class Solution {
         String str = Integer.toBinaryString(n);
         int r = 0;
         char[] ch = str.toCharArray();
-        char[] real = new char[31];
+        char[] real = new char[32];
 
-        char[] lastChar = new char[31];
+        char[] lastChar = new char[32];
         
         for (int i = 0; i < real.length; i++) {
             real[i] = '0';
         }
-        System.out.println(real);        
+        System.out.println(real);       
         
-        for (int i = 0; i < ch.length; i++) {
-            real[i] = ch[i];
+        int lenClearer = real.length - ch.length;
+        int realPos = 0;
+        
+        for (int i = lenClearer; i < real.length; i++) {
+            real[i] = ch[realPos++];
         }
         System.out.println(real);        
         
-        for (int i = 31; i >= 0; i--) {
+        for (int i = 31; i > 0; i--) {
             // 00000010100101000001111010011100
             lastChar[r] = real[i];
             r++;
