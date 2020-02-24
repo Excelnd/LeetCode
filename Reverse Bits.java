6cabc9720773bd7c0ddb1f7733805b20fe8227d2
Ihsan <ihsanmpm@gmail.com>
2020-02-24 16:30:37 +0530

    Update Reverse Bits.java

--- Reverse Bits.java
+++ Reverse Bits.java
@@ -5,9 +5,15 @@ public class Solution {
         System.out.println(str);
         int r = 0;
         char[] ch = str.toCharArray();
-        for (int i = 30; i >= 0; i--) {
+        char[] real = new real[31];
+        
+        for (int i = 0; i < ch.length; i++) {
+            real[i] = ch[i];
+        }
+        
+        for (int i = 31; i >= 0; i--) {
             // 00000010100101000001111010011100
-            ch[r] = str.charAt(i);
+            ch[r] = ch.charAt(i);
             r++;
         }
         
@@ -15,22 +21,4 @@ public class Solution {
         int rev = Integer.parseInt(fin, 2);
         return rev;
     }
-    /*
-    Input: 00000010100101000001111010011100
-Output: 00111001011110000010100101000000
-Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
-Example 2:
-
-Input: 11111111111111111111111111111101
-Output: 10111111111111111111111111111111
-Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 
-Note:
-
-Note that in some languages such as Java, there is no unsigned integer type. In this case, both input and output will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
-In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above the input represents the signed integer -3 and the output represents the signed integer -1073741825.
- 
-
-*/
-
-    
 }
