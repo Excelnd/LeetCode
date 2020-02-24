9cd616ccc41e7dc75d49c69e577185204bf37358
Ihsan <ihsanmpm@gmail.com>
2020-02-24 16:09:36 +0530

    Update Reverse Bits.java

--- Reverse Bits.java
+++ Reverse Bits.java
@@ -2,13 +2,16 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         String str = Integer.toBinaryString(n);
+        int r = 0;
         char[] ch = str.toCharArray();
         for (int i = str.length()-1; i <= 0; i--) {
-            ch[i] = str.charAt(i);
+            // 00000010100101000001111010011100
+            ch[r] = str.charAt(i);
+            r++;
         }
         
         String fin = new String(ch);
-        int rev = Integer.parseInt("fin", 2);
+        int rev = Integer.parseInt(fin, 2);
         return rev;
     }
     
