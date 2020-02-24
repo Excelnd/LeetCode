d4b788ba8df8ca7a7f5401a901245f923fe7abda
Ihsan <ihsanmpm@gmail.com>
2020-02-24 21:31:04 +0530

    Update Reverse Bits.java

--- Reverse Bits.java
+++ Reverse Bits.java
@@ -3,8 +3,11 @@ public class Solution {
     public int reverseBits(int n) {
         int rBits = 0;
         for (int i = 0; i < 32; i++) {
-            rBits = rBits + n & 1;
-            n = n >>> 1;      
+            rBits = rBits + (n & 1);
+            n = n >>> 1;
+            if (i < 31) {
+                rBits <<= 1;
+            }
         }
         return rBits;
     //  00000010100101000001111010011100   
