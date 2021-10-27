6415ae19ae7f6c23fa6a14ada26f17aba00290d1
Admin i <ihsanmpm@gmail.com>
2021-10-27 12:51:00 +0530

    Kth Largest Element in a Stream
    Time complexity: O(Nlog(N) + Mlog(k))
    
    Space complexity: O(N)O(N)

---
+++ src/Kth Largest Element in a Stream.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class KthLargest {
+	private PriorityQueue<Integer> pq;
+	private static int k;
+
+	public KthLargest(int k, int[] nums) {
+		this.k = k;
+		pq = new PriorityQueue<Integer>();
+
+		for (int num : nums) {
+			pq.offer(num);
+		}
+
+		while (pq.size() > k) {
+			pq.poll();
+		}
+
+	}
+
+	public int add(int val) {
+		pq.offer(val);
+
+		while (pq.size() > k) {
+			pq.poll();
+		}
+
+		System.out.println(pq.peek());
+		return pq.peek();
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 4, 5, 8, 2 };
+		KthLargest kthLargest = new KthLargest(3, nums);
+
+		kthLargest.add(3);
+		kthLargest.add(5);
+		kthLargest.add(10);
+		kthLargest.add(9);
+		kthLargest.add(4);
+
+	}
+
+}
--- src/ZigZag Conversion.java
+++ src/ZigZag Conversion.java
@@ -3,32 +3,34 @@ import java.util.List;
 
 class Solution1 {
 	public static String convert(String s, int numRows) {
-		
-		if (numRows == 1) return s;
-		
+
+		if (numRows == 1)
+			return s;
+
 		List<StringBuilder> rows = new ArrayList<>();
-		
-		for (int i = 0; i < Math.min(numRows, s.length()); i++) {	
+
+		for (int i = 0; i < Math.min(numRows, s.length()); i++) {
 			rows.add(new StringBuilder());
 		}
-		
+
 		int currentRow = 0;
 		boolean toBottom = false;
-		
-		for (char c: s.toCharArray()) {
+
+		for (char c : s.toCharArray()) {
 			rows.get(currentRow).append(c);
-			if (currentRow == 0 || currentRow == numRows - 1) 
+			if (currentRow == 0 || currentRow == numRows - 1)
 				toBottom = !toBottom;
 			currentRow += toBottom ? 1 : -1;
 		}
-		
+
 		StringBuilder ret = new StringBuilder();
-		for (StringBuilder row : rows) ret.append(row);
+		for (StringBuilder row : rows)
+			ret.append(row);
 		return ret.toString();
 
 	}
-	
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
 		System.out.println(convert("PAYPALISHIRING", 4));
 	}
 
