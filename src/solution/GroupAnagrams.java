143938a81148b84a6e98a582c7193db5c8b41601
Admin i <ihsanmpm@gmail.com>
2021-11-22 12:30:36 +0530

    Group Anagrams - by Sorting Strings

--- src/solution/GroupAnagrams.java
+++ src/solution/GroupAnagrams.java
@@ -1,10 +1,40 @@
 package solution;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class GroupAnagrams {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+    public List<List<String>> groupAnagrams(String[] strs) {
+    	
+    	if (strs == null || strs.length == 0) return new ArrayList<>();
+    	
+    	Map<String, List<String>> hashMap = new HashMap<>();
+    	
+    	for(String str : strs) {
+    		char[] charEachString = str.toCharArray();
+    		Arrays.sort(charEachString); // eat = aet, tea = aet, tan = ant, ate = aet, etc...,
+    		String keySortedString = String.valueOf(charEachString);
+    		
+    		if (!hashMap.containsKey(keySortedString)) {
+    			hashMap.put(keySortedString, new ArrayList<>());
+    		}
+    		hashMap.get(keySortedString).add(str);
 
+    	}
+
+		return new ArrayList<>(hashMap.values());        
+    }
+    
+    
+	public static void main(String[] args) {
+    	// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    	// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 	}
 
 }
