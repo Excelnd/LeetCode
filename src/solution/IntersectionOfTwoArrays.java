ec435cff0a8da12a8beb374f1584ea7941834134
Admin i <ihsanmpm@gmail.com>
2021-11-29 13:46:42 +0530

    IntersectionOfTwoArrays by BinarySearch with Sorted Array.

--- src/solution/IntersectionOfTwoArrays.java
+++ src/solution/IntersectionOfTwoArrays.java
@@ -2,32 +2,49 @@ package solution;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class IntersectionOfTwoArrays {
-
-	public int[] setIntersection(HashSet<Integer> set1, HashSet<Integer> set2) {
-
-		int[] result = new int[set1.size()];
-		int idx = 0;
-
-		for (Integer s : set1) {
-			if (set2.contains(s))
-				result[idx++] = s;			
-		}
-		return Arrays.copyOf(result, idx);
-	}
-
 	public int[] intersection(int[] nums1, int[] nums2) {
 
-		HashSet<Integer> setOne = new HashSet<Integer>();
-		for (Integer n : nums1)
-			setOne.add(n);
+		Set<Integer> hashSet = new HashSet<>();
 
-		HashSet<Integer> setTwo = new HashSet<Integer>();
-		for (Integer n : nums2)
-			setTwo.add(n);
+		Arrays.sort(nums2);
+
+		for (Integer num : nums1) {
+			if (binarySearch(nums2, num)) {
+				hashSet.add(num);
+			}
+		}
+
+		int i = 0;
+		int[] result = new int[hashSet.size()];
+
+		for (Integer num : hashSet) {
+			result[i++] = num;
+		}
+
+		return result;
+	}
+
+	public boolean binarySearch(int[] nums, int target) {
+		int low = 0;
+		int high = nums.length - 1;
 		
-		if (setOne.size() > setTwo.size()) return setIntersection(setOne, setTwo);
-		else return setIntersection(setTwo, setOne);
+		// nums1 = [4,9,5]
+		// nums2 = [9,4,9,8,4], Arrays.Sort(num2); -- [4,4,8,9,9]
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] == target) {
+				return true;
+			}
+			if (nums[mid] > target) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		return false;
 	}
 }
