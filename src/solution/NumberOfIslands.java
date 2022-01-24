56d587dec06459a7fd544541db04d789ef791d17
Admin i <ihsanmpm@gmail.com>
2022-01-24 11:30:29 +0530

    NumberOfIslands by DFS

--- src/solution/NumberOfIslands.java
+++ src/solution/NumberOfIslands.java
@@ -19,50 +19,39 @@ public class NumberOfIslands {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
 				if (grid[i][j] == '1') {
-					FillWaterBFS(grid, rows, cols, i, j);
+					FillWaterDFS(grid, i, j);
 					count++;
 				}
 			}
 		}
-
+		
+		System.out.println(count);
 		return count;
 
 	}
+	public void FillWaterDFS(char[][] grid, int i, int j) {
 
-	private final int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
-	public void FillWaterBFS(char[][] grid, int rows, int cols, int i, int j) {
-
-		Queue<Integer> queue = new LinkedList<>();
-
-		queue.offer(i * cols + j); // 2D array to 1D array : currRow * #cols + CurrCol
-
-		grid[i][j] = '0';
-
-		while (!queue.isEmpty()) {
-
-			int idx = queue.poll();
-
-			int row = idx / cols; // 1D array to 2D array for Row : currIndex / #cols
-			int col = idx % cols; // 1D array to 2D array for Col : currIndex % #cols
-
-			for (int[] dir : dirs) {
-				int x = dir[0] + row;
-				int y = dir[1] + col;
-
-				if (x > -1 && x < rows && y > -1 && y < cols && grid[x][y] == '1') {
-					grid[x][y] = '0';
-					queue.add(x * cols + y);
-				}
-			}
-
+		if(i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == '1') {
+			grid[i][j] = '0';	
+			FillWaterDFS(grid, i-1, j);			
+			FillWaterDFS(grid, i+1, j);			
+			FillWaterDFS(grid, i, j-1);			
+			FillWaterDFS(grid, i, j+1);			
 		}
 
 	}
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		char[][] grid = {
+				{'1','1','1','1','0'},
+				{'1','1','0','1','0'},
+				{'1','1','0','0','0'},
+				{'0','0','0','0','0'}
+				};
+		
+		new NumberOfIslands().numIslands(grid);
+		
 	}
 
 }
