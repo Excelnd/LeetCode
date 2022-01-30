2b284b0975f062ca619d106659d5e6e8d554cb81
Admin i <ihsanmpm@gmail.com>
2022-01-30 18:54:33 +0530

    Init. MaxAreaOfIsland - Depth-First Search (Iterative) Approach.

--- src/solution/MaxAreaOfIsland.java
+++ src/solution/MaxAreaOfIsland.java
@@ -2,29 +2,22 @@ package solution;
 
 public class MaxAreaOfIsland {
 	
-	boolean[][] alreadySeen;
 	
 	public int maxAreaOfIsland(int[][] grid) {
+		
+		boolean[][] alreadySeen = new boolean[grid.length][grid[0].length];
+		
+		int[] dirRow = { 1, -1, 0 , 0};
+		
+		int[] dirCol = { 0, 0, 1 , -1};
 
 		if (grid.length == 0)
 			return 0;
 
 		int maxArea = 0;
-
-		int r = grid.length;
-		int c = grid[0].length;
-
-		alreadySeen = new boolean[r][c];
-
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				if (grid[i][j] == 1) {
-					int area = DFScountArea(grid, i, j, r, c);
-					maxArea = Math.max(maxArea, area);
-				}
-			}
-		}
-
+		
+		
+		
 		return maxArea;
 
 		/*grid = 
@@ -38,21 +31,6 @@ public class MaxAreaOfIsland {
         [0,0,0,0,0,0,0,1,1,0,0,0,0]]
 */    
 	}
-    
-	public int DFScountArea(int[][] grid, int i, int j, int r, int c) {
-		
-		if (i < 0 || j < 0 || i >= r || j >= c || alreadySeen[i][j] || grid[i][j] == 0) {
-			return 0;
-		}
-		
-		alreadySeen[i][j] = true;
-		
-		int up = DFScountArea(grid, i-1, j, r, c);
-		int down = DFScountArea(grid, i+1, j, r, c);
-		int left = DFScountArea(grid, i, j-1, r, c);
-		int right = DFScountArea(grid, i, j+1, r, c);
-		
-		return (up + down + left + right + 1);
-	}
+
 
 }
