4aba7a78bf773d3a529a954f80b282b52ff24fdb
Admin i <ihsanmpm@gmail.com>
2022-03-17 12:58:23 +0530

    Merge Two Binary Trees - DFS Iterative

--- src/solution/MergeTwoBinaryTree.java
+++ src/solution/MergeTwoBinaryTree.java
@@ -1,43 +1,43 @@
 package solution;
 
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 
-public class MergeTwoBinaryTree {
+class MergeTwoBinaryTree {
 	public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        
-        if(root1 == null) return root2;
-     
-        Queue<TreeNode[]> q = new LinkedList<>();
-        
-        q.offer(new TreeNode[] {root1, root2});
 
-        
-        while (!q.isEmpty()) {
+		if (root1 == null)
+			return root2;
 
-            TreeNode[] nodes = q.poll();  
-            
-            if(nodes[1] == null) {
-                continue;
-            }
-            
-            nodes[0].val += nodes[1].val;
-            
-            if(nodes[0].left == null) {
-                nodes[0].left = nodes[1].left; 
-            } else {
-                q.offer(new TreeNode[] {nodes[0].left, nodes[1].left});
-            }
-            
-            if(nodes[0].right == null) {
-    
-                nodes[0].right = nodes[1].right;
-            } else {
-                q.offer(new TreeNode[] {nodes[0].right, nodes[1].right});
-            }
-           
-            
-        }
-        return root1;
+		Deque<TreeNode[]> stack = new LinkedList<>();
+
+		stack.offerLast(new TreeNode[] { root1, root2 });
+
+		while (!stack.isEmpty()) {
+
+			TreeNode[] cur = stack.pollLast();
+
+			if (cur[1] == null) {
+				continue;
+			}
+
+			cur[0].val += cur[1].val;
+
+			if (cur[0].left == null) {
+				cur[0].left = cur[1].left;
+			} else {
+				stack.offerLast(new TreeNode[] { cur[0].left, cur[1].left });
+			}
+
+			if (cur[0].right == null) {
+				cur[0].right = cur[1].right;
+			} else {
+				stack.offerLast(new TreeNode[] { cur[0].right, cur[1].right });
+			}
+
+		}
+
+		return root1;
 	}
+
 }
