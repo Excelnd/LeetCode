442005a7fda07a7da964b7f74ed124f2d7cde005
Admin i <ihsanmpm@gmail.com>
2022-03-18 11:55:59 +0530

    Merge Two Binary Trees - Recursive

--- src/solution/MergeTwoBinaryTree.java
+++ src/solution/MergeTwoBinaryTree.java
@@ -5,36 +5,17 @@ import java.util.LinkedList;
 
 class MergeTwoBinaryTree {
 	public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-
-		if (root1 == null)
+		if (root1 == null) {
 			return root2;
+		}
 
-		Deque<TreeNode[]> stack = new LinkedList<>();
+		if (root2 != null) {
+			
+			root1.val += root2.val;
+			
+			root1.left = mergeTrees(root1.left, root2.left);
 
-		stack.offerLast(new TreeNode[] { root1, root2 });
-
-		while (!stack.isEmpty()) {
-
-			TreeNode[] cur = stack.pollLast();
-
-			if (cur[1] == null) {
-				continue;
-			}
-
-			cur[0].val += cur[1].val;
-
-			if (cur[0].left == null) {
-				cur[0].left = cur[1].left;
-			} else {
-				stack.offerLast(new TreeNode[] { cur[0].left, cur[1].left });
-			}
-
-			if (cur[0].right == null) {
-				cur[0].right = cur[1].right;
-			} else {
-				stack.offerLast(new TreeNode[] { cur[0].right, cur[1].right });
-			}
-
+			root1.right = mergeTrees(root1.right, root2.right);
 		}
 
 		return root1;
