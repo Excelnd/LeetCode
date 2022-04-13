ba0353460199233ce39ad12cd15517f58c66b76e
Admin i <ihsanmpm@gmail.com>
2022-04-13 14:00:08 +0530

    Binary Tree Zigzag Level Order Traversal - with ArrayList add

---
+++ src/solution/ZigzagLevelOrderTraversal.java
@@ -0,0 +1,52 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigzagLevelOrderTraversal {
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		List<List<Integer>> res = new LinkedList<>();
+
+		Queue<TreeNode> queue = new LinkedList<>();
+
+		if (root == null) {
+			return res;
+		}
+
+		queue.add(root);
+
+		boolean zigzagFlag = false;
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> eachLevel = new ArrayList<>();
+
+			for (int i = 0; i < size; i++) {
+
+				TreeNode node = queue.poll();
+
+				if (zigzagFlag) {
+					eachLevel.add(0, node.val);
+				}
+
+				else {
+					eachLevel.add(node.val);
+				}
+
+				if (node.left != null) {
+					queue.add(node.left);
+				}
+
+				if (node.right != null) {
+					queue.add(node.right);
+				}
+			}
+			res.add(eachLevel);
+			zigzagFlag = !zigzagFlag;
+		}
+
+		return res;
+	}
+}
