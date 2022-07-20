712de42c165b866dbd5ff8f2d5eb3e656898bbc2
Admin i <ihsanmpm@gmail.com>
2022-07-20 13:01:14 +0530

    v2 Two Sum II

--- src/solution/TwoSum_II.java
+++ src/solution/TwoSum_II.java
@@ -2,17 +2,21 @@ package solution;
 
 public class TwoSum_II {
 	public int[] twoSum(int[] numbers, int target) {
+        
+        int[] index = new int[2];
+        if (numbers.length < 2) return index;
 
 		int leftPointer = 0;
 		int rightPointer = numbers.length - 1;
-		int sum = 0;
 
 		while (leftPointer < rightPointer) {
 
-			sum = numbers[leftPointer] + numbers[rightPointer];
+			int sum = numbers[leftPointer] + numbers[rightPointer];
 
 			if (target == sum) {
-				return new int[] { leftPointer + 1, rightPointer + 1 };
+				index[0] = leftPointer + 1;
+                index[1] = rightPointer + 1;
+                break;
 			}
 			if (sum >= target) {
 				rightPointer--;
@@ -22,6 +26,6 @@ public class TwoSum_II {
 			} 
 
 		}
-		return new int[] { 0, 0 };
+		return index;
 	}
 }
